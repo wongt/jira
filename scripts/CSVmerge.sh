e42bb706ca7cfa59699f630695cef82474953401
Teddy Wong <wong.teddy@gmail.com>
2019-12-09 15:52:24 -0500

    Create CSVmerge.sh
    Script to merge multiple CSV files.

---
+++ scripts/CSVmerge.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dir=''
+OutFileName=$dir/RESULT_`date +"%d-%m-%Y"`.csv                       # Fix the output name
+i=0                                       # Reset a counter
+for filename in "$dir/"*".csv"; do
+ if [ "$filename" != "$OutFileName" ] ;      # Avoid recursion 
+ then 
+   if [[ $i -eq 0 ]] ; then 
+      head -1  "$filename" > "$OutFileName" # Copy header if it is the first file
+   fi
+   tail -n +2  "$filename" >> "$OutFileName" # Append from the 2nd line each file
+   i=$(( $i + 1 ))                        # Increase the counter
+ fi
+done
